c635a0f4c4ad2886086d0d24ac36c6c07c20b4ed
Punit Shah <punit@punitshah.dev>
2019-11-07 19:05:54 +0000

    Loading spinner for results

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Router from 'next/router'
 import moment from 'moment-mini'
+import MoonLoader from 'react-spinners/MoonLoader'
 
 import { getCoordsFromLocation } from '../utils/google'
 import { getStashpoints } from '../utils/api'
@@ -8,15 +9,17 @@ import Layout from '../components/Layout'
 import { SearchBar } from '../components/SearchBar'
 import { Stashpoint } from '../components/Stashpoint'
 
-import { box } from '../style/style.css'
+import { box, resultsContainer } from '../style/style.css'
 
 const Home = () => {
   const [results, setResults] = useState([])
   const [bags, setBags] = useState(1)
   const [dropOff, setDropOff] = useState(moment())
   const [pickUp, setPickUp] = useState(moment().add(1, 'hours'))
+  const [loading, setLoading] = useState(false)
 
-  const updateResults = async (searchVal) => {
+  const updateResults = async searchVal => {
+    setLoading(true)
     const resGeo = await getCoordsFromLocation(searchVal)
 
     const coords = resGeo.results[0].geometry.location
@@ -27,10 +30,11 @@ const Home = () => {
       pickUp: pickUp.format('YYYY-MM-DDTHH:mm')
     })
 
+    setLoading(false)
     setResults(resStash)
   }
 
-  const onBook = (id) => {
+  const onBook = id => {
     Router.push(
       {
         pathname: '/checkout',
@@ -59,10 +63,16 @@ const Home = () => {
         />
       </header>
 
-      <div>
-        <ul>
-          {results.map(r => <Stashpoint key={r.id} data={r} onBook={() => onBook(r.id) } />)}
-        </ul>
+      <div className={resultsContainer}>
+        {loading ? (
+          <MoonLoader size={32} color="#000" />
+        ) : (
+          <ul>
+            {results.map(r => (
+              <Stashpoint key={r.id} data={r} onBook={() => onBook(r.id)} />
+            ))}
+          </ul>
+        )}
       </div>
     </Layout>
   )
