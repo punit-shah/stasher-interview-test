67e96356ddc8725c55efc3845e08d56bfa92b165
Punit Shah <punit@punitshah.dev>
2019-11-07 04:24:31 +0000

    Make booking/payment api calls

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,6 @@
 import moment from 'moment-mini'
 import cookie from 'js-cookie'
-import { getStashpoint, getQuote } from '../utils/api'
+import { getStashpoint, getQuote, makeBooking, finalizePay } from '../utils/api'
 import StashpointInfo from '../components/StashpointInfo'
 import '../style/global.css'
 import { title, page, twoColumn } from '../style/style.css'
@@ -15,7 +15,16 @@ const Checkout = ({
   currentPath,
   currentQuery
 }) => {
-  const isLoggedIn = !!cookie.get('st_token')
+  const token = cookie.get('st_token')
+  const onBook = async () => {
+    const booking = await makeBooking({ bags, dropOff, pickUp, stashpointId: stashpoint.id }, token)
+    console.log(booking)
+
+    const pay = await finalizePay({ booking_id: booking.id }, token)
+    console.log(pay)
+  }
+  const isLoggedIn = !!token
+
   return (
     <div className={page}>
       <h1 className={title}>Stasher</h1>
@@ -30,6 +39,7 @@ const Checkout = ({
           isLoggedIn={isLoggedIn}
           currentPath={currentPath}
           currentQuery={currentQuery}
+          onBook={onBook}
         />
       </div>
     </div>
