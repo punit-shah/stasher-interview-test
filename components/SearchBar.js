7f39a65886baa93f11520db6115e51034292f52a
Punit Shah <punit@punitshah.dev>
2019-11-07 20:21:25 +0000

    Make SearchBar a form, prevent empty search

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -4,33 +4,62 @@ import { BagSelector } from './BagSelector'
 import { DateSelector } from './DateSelector'
 
 import { item, hr } from '../style/style.css'
-import { filtersWrapper } from './SearchBar.css'
-
+import { searchForm, filtersWrapper } from './SearchBar.css'
 
 const bagOptions = Array.apply(null, Array(20)).map((x, i) => i)
 
-
-
-export const SearchBar = ({ bags, onSelectBags, dropOff, onChangeDropOff, pickUp, onChangePickUp, onSearch }) => {
-  const [searchVal, setSearchVal] = useState('london')
+export const SearchBar = ({
+  bags,
+  onSelectBags,
+  dropOff,
+  onChangeDropOff,
+  pickUp,
+  onChangePickUp,
+  onSearch
+}) => {
+  const [searchVal, setSearchVal] = useState('London')
 
   return (
-    <>
+    <form
+      className={searchForm}
+      onSubmit={event => {
+        event.preventDefault()
+        onSearch(searchVal)
+      }}
+    >
       <input
         className={item}
         type="search"
-        onChange={({ target: { value } }) => setSearchVal(value)} value={searchVal}
+        onChange={({ target: { value } }) => setSearchVal(value)}
+        value={searchVal}
+        required
       />
-      <button className={item} onClick={() => onSearch(searchVal)}>Search</button>
+      <button type="submit" className={item}>
+        Search
+      </button>
 
       <hr className={hr} />
 
       <div className={filtersWrapper}>
-        <BagSelector className={item} value={bags} options={bagOptions} onSelect={onSelectBags}/>
-
-        <DateSelector className={item} label="Drop off" value={dropOff} onChange={onChangeDropOff} />
-        <DateSelector className={item} label="Pick up" value={pickUp} onChange={onChangePickUp} />
+        <BagSelector
+          className={item}
+          value={bags}
+          options={bagOptions}
+          onSelect={onSelectBags}
+        />
+        <DateSelector
+          className={item}
+          label="Drop off"
+          value={dropOff}
+          onChange={onChangeDropOff}
+        />
+        <DateSelector
+          className={item}
+          label="Pick up"
+          value={pickUp}
+          onChange={onChangePickUp}
+        />
       </div>
-    </>
+    </form>
   )
 }
